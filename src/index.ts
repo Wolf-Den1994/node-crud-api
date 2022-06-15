58eeb26e2cdd39c51c2e46b4bee8c9da44d0e3f9
Denis <wolf-den@tut.by>
2022-06-15 22:34:37 +0300

    feat: createServer

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
-console.log(1);
+import { v4 as uuidv4 } from 'uuid';
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+import { IUser } from './types';
+
+const users: IUser[] = [
+  {
+    id: uuidv4(),
+    username: 'name',
+    age: 22,
+    hobbies: ['hobby1', 'hobby2'],
+  },
+];
+
+const PORT = process.env.PORT || 4000;
+
+const server = createServer((req: IncomingMessage, res: ServerResponse) => {
+  const route = req.url?.replace(/\/$|\/*$/g, '');
+  const method = req.method || '';
+
+  if (route === '/api/users') {
+    switch (method) {
+      case 'GET':
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(users));
+        break;
+      default:
+        res.writeHead(500, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'Ooops, Something Wrong' }));
+        break;
+    }
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Route Not Found' }));
+  }
+});
+
+server.listen(PORT, () => console.log(`Server is runnig on port ${PORT}`));
